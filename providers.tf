8ae0fbd7776c981a500796f4d0cf20fe0c4ce235
abhi-biradar <200432.abhishek@gmail.com>
2024-12-29 11:13:59 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIAZDZTBR5CP7FSOF7V"
+  secret_key = "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"
 }
