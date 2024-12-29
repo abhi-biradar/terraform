a68bacf695f40150cc5d8841b0fc1abd10dfc0c9
abhi-biradar <200432.abhishek@gmail.com>
2024-12-29 10:56:43 +0530

    Update web_sg.tf

--- web_sg.tf
+++ web_sg.tf
@@ -5,16 +5,16 @@ resource "aws_security_group" "demosg" {
   # Inbound rules
   # HTTP access from anywhere
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   # HTTPS access from anywhere
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 1010
+    to_port     = 1010
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
